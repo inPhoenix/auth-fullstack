754b69274d0d74e2e362e7dd1493e9647f09225b
inPhoenix <spiderk@gmail.com>
2019-03-14 22:03:38 +0000

    Updating the UI and style files.

--- client/src/app/scenes/Login.tsx
+++ client/src/app/scenes/Login.tsx
@@ -9,15 +9,25 @@ import styled from "styled-components"
 const ASSETS = `${process.env.PUBLIC_URL}/assets`
 
 interface IMyImageProps {
-  back?: string,
+  back?: string
 }
 
 const Header = styled.div<IMyImageProps>`
-width: 50%;
-background: linear-gradient(rgba(35,39,72,0.9), rgba(35,39,72,0.4)), url(${props => props.back});
+  width: 50%;
+  box-shadow: -10px 0 30px rgba(0, 0, 0, 0.3);
+  background: linear-gradient(
+      to bottom,
+      rgba(9, 0, 72, 0.9),
+      rgba(9, 0, 72, 0.4)
+    ),
+    url(${props => props.back}) center no-repeat;
 `
 
-interface IStateFromProps {}
+interface IStateFromProps {
+  value?: any
+  showLogin: any
+  checkbox: any
+}
 
 interface HomeProps extends RouteComponentProps {
   changeTheme(): void
@@ -30,21 +40,57 @@ class Login extends Component<IProps> {
     this.props.changeTheme()
   }
 
+  state = {
+    showLogin: false,
+    checkbox: false
+  }
+
+  enableRegister = () => {
+    console.log("%c hi", "background: red")
+    this.setState(() => ({
+      showLogin: true
+    }))
+  }
+
+  disableRegister = () => {
+    console.log("%c hi", "background: red")
+    this.setState(() => ({
+      showLogin: false
+    }))
+  }
+
+  handleOnChange = () => {
+    this.setState((prevState: any) => ({
+      showLogin: !prevState.checkbox
+    }))
+  }
+
   render() {
+    const { showLogin, checkbox } = this.state
+    console.log("%c showLogin", "background: red", showLogin)
+    console.log("%c checkbox", "background: red", checkbox)
+    // @ts-ignore
+    // @ts-ignore
     return (
       <div>
         <LoginContainer back={`${ASSETS}/wallpapers/bg1.jpg`} />
 
         <div className="login-container">
-          <div className="banner">
+          <div className={["banner", showLogin && "disable"].join(" ")}>
             <h1 className="banner-heading">Subscribe Here</h1>
             <div className="arrow">
               <i className="fas fa-arrow-down" />
             </div>
-            <button className="banner-btn">click now</button>
+            <button
+              type="button"
+              className="banner-btn"
+              onClick={this.enableRegister}
+            >
+              click now
+            </button>
           </div>
 
-          <div className="form-container">
+          <div className={["form-container", showLogin && "enable"].join(" ")}>
             <form action="" className="sign-up-form">
               <input
                 type="text"
@@ -56,19 +102,21 @@ class Login extends Component<IProps> {
                 className="form-input"
                 placeholder="Email Address"
               />
-              <p>Get News?</p>
+              <p>Already Have an Account?</p>
               <label>No</label>
-              <input type="checkbox" />
+              <input onChange={this.handleOnChange} type="checkbox" />
               <label>Yes</label>
               <button type="button">Sign Up Here</button>
             </form>
 
-            <Header back={`${ASSETS}/wallpapers/mobilelogin.jpg`}>
-              Welcome to Registration
+            <Header
+              className="header"
+              back={`${ASSETS}/wallpapers/mobilelogin.jpg`}
+            >
+              <div className="header-inner">Auth System</div>
             </Header>
-            <div className="x-btn">
-
-            &#10005;
+            <div className="x-btn" onClick={this.disableRegister}>
+              &#10005;
             </div>
           </div>
         </div>
